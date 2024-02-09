bfede9667cb6073c8a50f154bf86cbecbef6ebdf
ThanathornJung <thanathorn.workspace@gmail.com>
2024-02-09 19:17:59 +0700

    GameSystem

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -15,6 +15,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake() {
         Instance = this;
@@ -62,6 +63,8 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe) {
                     // Player delivered the correct recipe!
 
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -77,5 +80,9 @@ public class DeliveryManager : MonoBehaviour
 
     public List<RecipeSO> GetWaitingRecipeSOList() {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount() {
+        return successfulRecipesAmount;
     }
 }
