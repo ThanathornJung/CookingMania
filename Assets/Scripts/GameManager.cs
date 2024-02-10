0a754e906c49183f0aa496a1e3740000e289da97
ThanathornJung <thanathorn.workspace@gmail.com>
2024-02-10 19:50:24 +0700

    Polish2nd

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 120f;
+    private float gamePlayingTimerMax = 90f;
     private bool isGamePaused = false;
 
     private void Awake() {
@@ -67,7 +67,6 @@ public class GameManager : MonoBehaviour
             case State.GameOver:
                 break;
         }
-        Debug.Log(state);
     }
 
     public bool IsGamePlaying() {
--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
+    [SerializeField] private Button mainMenuButton;
+
     [SerializeField] private TextMeshProUGUI recipesDeliveredText;
+
+    private void Awake() {
+        mainMenuButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+    }
 
     private void Start() {
         GameManager.Instance.OnStageChanged += GameManager_OnStageChanged;
